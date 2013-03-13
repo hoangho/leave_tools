08fa42dcd2dc8a61b06b40bbd8270749732ba027
hoangho <hoang.ho@techpropulsionlabs.com>
2013-03-13 09:45:27 +0700

    Init project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LeaveTools::Application.config.secret_token = '0bff560e1ebdc3c66701e6da7ecd822958e7306a0da6dfcf8b89057a9fdb245414beae76dc91a6be3224a18dc6be1c73a86642fc4107d35360d8e826baa0652c'
