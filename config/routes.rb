4bbb453dc752689597b6a12ce271f436a130e319
hoangho <hoang.ho@techpropulsionlabs.com>
2013-03-13 11:50:09 +0700

    finish sign_in + sign_out + sign_up user

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 LeaveTools::Application.routes.draw do
   resources :users
+  resources :sessions, only: [:new, :create, :destroy]
 
   # get "static_pages/home"
 
@@ -14,7 +15,11 @@ LeaveTools::Application.routes.draw do
   match '/help',    to: 'static_pages#help'
   match '/about',   to: 'static_pages#about'
   match '/contact', to: 'static_pages#contact'  
-  match '/add_user',  to: 'users#new'
+
+  # match '/add_user',  to: 'users#new'
+  match '/signup', to: 'users#new'
+  match '/signin',  to: 'sessions#new'
+  match '/signout', to: 'sessions#destroy', via: :delete
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
